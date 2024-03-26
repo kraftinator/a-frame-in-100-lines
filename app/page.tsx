ea7c2a94bdc139746cabcecd15427bdc2c1b18c9
Adam Kraft <adamkraft1@gmail.com>
2024-03-26 15:50:27 -0500

    First commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Story time',
+      label: 'Spartacus The Cat',
     },
     {
       action: 'tx',
@@ -40,7 +40,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <h1>zizzamia.xyz</h1>
+      <h1>kraftinator</h1>
     </>
   );
 }
